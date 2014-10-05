b0b585c79213a2105e250bdd622a8b408dafa857
quazzer <quazzer@QuAzZeR>
2014-10-05 11:12:18 +0700

    Create BingBall class and extends BasicGame

---
+++ bingball/src/bingball/BingBall.java
@@ -0,0 +1,45 @@
+package bingball;
+
+import java.util.prefs.BackingStoreException;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class BingBall extends BasicGame{
+
+	public BingBall(String title)
+	{
+		super(title);
+	}
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		try {
+		      BingBall game = new BingBall("Bing Ball");
+		      AppGameContainer appgc = new AppGameContainer(game);
+		      appgc.setDisplayMode(320, 480, false);
+		      appgc.start();
+		    } catch (SlickException e) {
+		      e.printStackTrace();
+		    }
+
+	}
+}
